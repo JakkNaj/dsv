546a4ab507a0ad3f09aadeea91b2bc9958fd582d
Jakub Najman <kubanaj@gmail.com>
2024-12-09 21:05:35 +0100

    connect to rabbitmq

--- src/main/java/com/dsv/Main.java
+++ src/main/java/com/dsv/Main.java
@@ -5,6 +5,9 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Enumeration;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 
 public class Main {
     private static String getServerIp() {
@@ -35,7 +38,35 @@ public class Main {
                 System.out.println("Endpoint byl zavolán!");
                 ctx.result("Hello World from Javalin!");
             })
+            .post("/rabbitmq", ctx -> {
+                System.out.println("Endpoint rabbitmq byl zavolán!");
+                try {
+                    ConnectionFactory factory = new ConnectionFactory();
+                    factory.setHost("192.168.64.4");
+                    factory.setPort(5672);
+                    factory.setUsername("guest");
+                    factory.setPassword("guest");
+
+                    Connection connection = factory.newConnection();
+                    Channel channel = connection.createChannel();
+                    
+                    String queueName = "test-queue";
+                    channel.queueDeclare(queueName, false, false, false, null);
+                    
+                    String message = "Message from /rabbitmq endpoint";
+                    channel.basicPublish("", queueName, null, message.getBytes());
+                    
+                    channel.close();
+                    connection.close();
+                    
+                    ctx.result("success");
+                } catch (Exception e) {
+                    System.err.println("Chyba při odesílání do RabbitMQ: " + e.getMessage());
+                    ctx.result("error");
+                }
+            })
             .start(7070);
+
 
         System.out.println("Server běží na http://" + serverIp + ":7070");
     }
